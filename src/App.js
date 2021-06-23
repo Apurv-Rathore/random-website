d8a224111847b1d6a582595dd899d993427c64f6
apurvr-newzera <2019csb1077@iitrpr.ac.in>
2021-06-23 21:29:05 +0530

    ff

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import MainBody from "./components/MainBody"
+import logo from "./logo.svg";
+import "./App.css";
+import gmail from "./icons/gmail.png"
+import MainBody from "./components/MainBody";
 function App() {
-  
   return (
-    <div className="App"  >
-      <MainBody/>
+    <div className="App">
+      <MainBody />
+      <footer className="footer" style={{position:'absolute',top:"45vh",width:'100vw',alignContent:'center'}}>
+        <span style={{paddingRight:"100px"}}>
+        To advertise
+        </span>
+
+      
+
+        <span>
+        
+        <a href="mailto:developerapurv@gmail.com" style={{marginLeft:"-100px"}}><img src={gmail} style={{height:"30px",width:"40px"}} alt="gfs" /></a>
+        </span>
+      </footer>
     </div>
   );
 }
-
 export default App;
