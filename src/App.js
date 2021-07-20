92daf8619b9396a8dd4576720e51df52afb8dd51
apurvr-newzera <2019csb1077@iitrpr.ac.in>
2021-07-20 15:09:49 +0530

    final

--- src/App.js
+++ src/App.js
@@ -14,21 +14,61 @@ function App() {
     bottom: "0",
     height: "0px",
     width: "100%",
-}
+  };
 
-var phantom = {
-  display: 'block',
-  padding: '20px',
-  height: '60px',
-  width: '100%',
-}
+  var phantom = {
+    display: "block",
+    padding: "20px",
+    height: "60px",
+    width: "100%",
+  };
   return (
     <div className="App">
       <div className="back">
         {/* <video src="WeirdAnimatedGifs" type="gif"></video> */}
         {/* <img src={WeirdAnimatedGifs} alt="fuck" /> */}
         <MainBody />
-        <footer
+        <div
+          style={{
+            alignContent: "center",
+            marginTop: "80px",
+          }}
+        >
+          <span style={{fontFamily: "Kirang Haerang,cursive", fontSize:"25px"}}>To advertise, Mail me</span>
+          <br />
+          {/* <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br /><br /> */}
+          <span>
+            <a
+              href="mailto:developerapurv@gmail.com"
+            >
+              <img
+                src={gmail}
+                style={{ height: "40px", width: "50px" }}
+                alt="gfs"
+              />
+            </a>
+          </span>
+        </div>
+        <footer 
+        style={{position:"absolute",backgroundColor:"rgb(0, 194, 253)",
+        top:"73vh",width:"100vw"}}>
+          {/* Made with <span style="color: #e25555;">&#9829;</span> in Switzerland */}
+          {/* <span>
+    Made with <i class="fa fa-heart pulse"></i> in <a href="https://www.google.de/maps/place/Augsburger+Puppenkiste/@48.360357,10.903245,17z/data=!3m1!4b1!4m2!3m1!1s0x479e98006610a511:0x73ac6b9f80c4048f" target="_blank">Auxburg</a>
+</span> */}
+
+<span style={{fontFamily: "Kirang Haerang,cursive"}}>
+  
+Made with <span style={{color:"red"}}>♥</span>  in India
+</span>
+        </footer>
+        {/* <footer
           className="footer"
           style={{
             position: "absolute",
@@ -49,7 +89,7 @@ var phantom = {
               />
             </a>
           </span>
-        </footer>
+        </footer> */}
       </div>
     </div>
   );
